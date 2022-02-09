9cd0802af42c52825c450812d9122acf28855a4c
Teppei Fukuda <knqyf263@gmail.com>
2022-02-09 12:16:02 +0200

    refactor: use multierror (#13)

--- cmd/rpmdb/main.go
+++ cmd/rpmdb/main.go
@@ -4,24 +4,24 @@ import (
 	"fmt"
 	"log"
 
+	multierror "github.com/hashicorp/go-multierror"
 	rpmdb "github.com/knqyf263/go-rpmdb/pkg"
 )
 
 func main() {
-	db, err := rpmdb.Open("./rpmdb.sqlite")
-	if err != nil {
-		db, err = rpmdb.Open("./Packages.db")
-	}
-	if err != nil {
-		db, err = rpmdb.Open("./Packages")
-	}
-
-	if err != nil {
+	if err := run(); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func run() error {
+	db, err := detectDB()
+	if err != nil {
+		return err
 	}
 	pkgList, err := db.ListPackages()
 	if err != nil {
-		log.Fatal(err)
+		return err
 	}
 
 	fmt.Println("Packages:")
@@ -34,4 +34,29 @@ func main() {
 		fmt.Printf("\t%+v\n", *pkg)
 	}
 	fmt.Printf("[Total Packages: %d]\n", len(pkgList))
+
+	return nil
+}
+
+func detectDB() (*rpmdb.RpmDB, error) {
+	var result error
+	db, err := rpmdb.Open("./rpmdb.sqlite")
+	if err == nil {
+		return db, nil
+	}
+	result = multierror.Append(result, err)
+
+	db, err = rpmdb.Open("./Packages.db")
+	if err == nil {
+		return db, nil
+	}
+	result = multierror.Append(result, err)
+
+	db, err = rpmdb.Open("./Packages")
+	if err == nil {
+		return db, nil
+	}
+	result = multierror.Append(result, err)
+
+	return nil, result
 }
